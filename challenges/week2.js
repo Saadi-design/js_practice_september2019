edbfbeee8c135a8406397edf70c9f93721585546
Saadiya Moid <saadiyamoid@gmail.com>
2019-11-24 22:03:23 +0000

    week 2 except last one

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,32 +1,37 @@
-// function getFillings(sandwich) {
-//   if (sandwich === undefined) throw new Error("ingredients is required");
-//   // Your code here!
-// }
+function getFillings(sandwich) {
+  if (sandwich === undefined) throw new Error("ingredients is required");
+ return sandwich.fillings
+}
 
-// function isFromManchester(person) {
-//   if (person === undefined) throw new Error("person is required");
-//   // Your code here!
-// }
+function isFromManchester(person) {
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester"
+}
 
-// function getBusNumbers(people) {
-//   if (people === undefined) throw new Error("people is required");
-//   // Your code here!
-// }
+function getBusNumbers(people) {
+  if (people === undefined) throw new Error("people is required");
+  return Math.ceil(people / 40);
+}
 
-// function countSheep(arr) {
-//   if (arr === undefined) throw new Error("arr is required");
-//   // Your code here!
-// }
+function countSheep(arr) {
+  if (arr === undefined) throw new Error("arr is required");
+  arr.forEach(function (item) {
+    if (item === "sheep") {
+      total++;
+    }
+  })
+  return total;
+}
 
-// function hasMPostCode(person) {
-//   if (person === undefined) throw new Error("person is required");
-//   // Your code here!
-// }
+function hasMPostCode(person) {
+  if (person === undefined) throw new Error("person is required");
+  
+}
 
-// module.exports = {
-//   getFillings,
-//   isFromManchester,
-//   countSheep,
-//   getBusNumbers,
-//   hasMPostCode
-// };
+module.exports = {
+  getFillings,
+  isFromManchester,
+  countSheep,
+  getBusNumbers,
+  hasMPostCode
+};
